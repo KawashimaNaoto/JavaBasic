9f459d435285cbb573f9e1fb210750d588aa3fe2
DESKTOP-JK415VH\naxtt <naoto.kawashima.likeit@gmail.com>
2018-10-25 15:42:18 +0900

    13-9

--- Practice/src/practice13/ptra13/PTra13_09.java
+++ Practice/src/practice13/ptra13/PTra13_09.java
@@ -6,6 +6,8 @@
  */
 package practice13.ptra13;
 
+import practice13.common.Employee;
+
 public class PTra13_09 {
 
 	/*
@@ -35,5 +37,28 @@ public class PTra13_09 {
 
 		// ★ 定数で定義されている各データを使用して、Employeeインスタンスを３つ作成してください
 
+		Employee employee1 = new Employee();
+
+			employee1.setDepartmentNm(QUATERDATA[0][0]);
+			System.out.println(employee1.getDepartmentNm());
+			employee1.setDepartmentCnt(Integer.parseInt(QUATERDATA[1][0]));
+			System.out.println(employee1.getDepartmentCnt());
+			System.out.println(NAMEDATA[0]);
+			System.out.println(MAILDATA[0]);
+			System.out.println(PASSDATA[0]);
+
+		Employee employee2 = new Employee();
+			employee2.setDepartmentNm(QUATERDATA[0][1]);
+			System.out.println(employee2.getDepartmentNm());
+			System.out.println(NAMEDATA[1]);
+			System.out.println(MAILDATA[1]);
+			System.out.println(PASSDATA[1]);
+
+		Employee employee3 = new Employee();
+			employee3.setDepartmentNm(QUATERDATA[0][2]);
+			System.out.println(employee3.getDepartmentNm());
+			System.out.println(NAMEDATA[2]);
+			System.out.println(MAILDATA[2]);
+			System.out.println(PASSDATA[2]);
 	}
 }
