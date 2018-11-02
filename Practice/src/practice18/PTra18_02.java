87b10572fa17068b77121d4c6ddf3fd097606120
DESKTOP-JK415VH\naxtt <naoto.kawashima.likeit@gmail.com>
2018-11-02 18:08:06 +0900

    18-2

--- Practice/src/practice18/PTra18_02.java
+++ Practice/src/practice18/PTra18_02.java
@@ -6,6 +6,11 @@
  */
 package practice18;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class PTra18_02 {
 
 	/*
@@ -31,10 +36,31 @@ public class PTra18_02 {
 		 * ★ file/BestElevenCandidate.csvの内容を取得し、１行毎にPlayerインスタンスに情報を格納してください
 		 * ★ ArrayListを作成して、Playerインスタンスを格納してください
 		 */
+		ArrayList<Player> array = new ArrayList<>();
+		try (Scanner scanner = new Scanner(new File("file/BestElevenCandidate.csv"))) {
+			while (scanner.hasNext()) {
+				String line = scanner.nextLine();
 
+				String[] x = line.split(",");
+
+				Player player = new Player();
+				player.setPosition(x[0]);
+				player.setName(x[1]);
+				player.setCountry(x[2]);
+				player.setTeam(x[3]);
+
+				array.add(player);
+
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("ファイルが見つかりません");
+		}
 
 		// ★ ArrayListに格納されているインスタンス全てのtoStringメソッドを実行し、出力してください
 		// ※ できれば拡張for文を使いましょう
 
+		for (Player x : array) {
+			System.out.println(array);
+		}
 	}
 }
